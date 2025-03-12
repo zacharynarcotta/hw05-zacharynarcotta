a1055dba4b83e27c2e2c1dc9a6ec33f87ae20fbb
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-12 13:04:24 -0400

    Create parameterized constructor and ship method

--- CustomerOrder.java
+++ CustomerOrder.java
@@ -8,4 +8,18 @@ public class CustomerOrder {
         date = null;
     }
 
+    public CustomerOrder(String name, String date, int quantity) {
+        this.name = name;
+        this.date = date;
+        this.quantity = quantity;
+    }
+
+    void ship(int quantity) {
+        if(this.quantity - 1 < 0) {
+            return;
+        }
+        else {
+            this.quantity--;
+        }
+    }
 }
