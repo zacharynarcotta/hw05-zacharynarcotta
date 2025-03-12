f7a8973652d7ecc6af03ac930519d285ecab146c
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-12 13:02:18 -0400

    Create instance variables and null constructor

--- CustomerOrder.java
+++ CustomerOrder.java
@@ -1,3 +1,11 @@
 public class CustomerOrder {
-    
+    public String name;
+    public String date;
+    public int quantity = 0;
+
+    public CustomerOrder() {
+        name = null;
+        date = null;
+    }
+
 }
