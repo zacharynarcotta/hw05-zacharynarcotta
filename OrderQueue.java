2ea4242b075aa728164d600b3465a0dedcfc3310
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-25 18:47:59 -0400

    Add required methods and toString Override

--- OrderQueue.java
+++ OrderQueue.java
@@ -6,7 +6,40 @@ public class OrderQueue {
     public OrderQueue() {
         stock = 0;
     }
+
     public OrderQueue(int stock) {
         this.stock = stock;
+    }
+
+    public void addOrder(String name, String date, int quantity) {
+        CustomerOrder thisOrder = new CustomerOrder(name, date, quantity);
+        this.queue.enqueue(thisOrder);
+    }
+
+    public void addStock(int addStock) {
+        this.stock += addStock;
+    }
+
+    public void fillOrder() {
+        CustomerOrder frontOrder = this.queue.getFront();
+        frontOrder.quantity--;
+        this.stock--;
+        if(frontOrder.quantity <= 0) {
+            this.queue.dequeue();
+        }
+    }
+
+    public void sellStock() {
+        CustomerOrder frontOrder = this.queue.getFront();
+        while(this.stock > 0) {
+            this.stock--;
+            frontOrder.quantity++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        CustomerOrder frontOrder = this.queue.getFront();
+        return "" + frontOrder.quantity;
     }
 }
