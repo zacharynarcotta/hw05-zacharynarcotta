5e1e2269ecd43a87e2f74f40d6b2f0783c117834
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-25 19:03:56 -0400

    Create tester file as outlined in PDF

--- OrderTester.java
+++ OrderTester.java
@@ -1,3 +1,19 @@
 public class OrderTester {
-    
+    public static void main(String[] args) {
+        OrderQueue queue = new OrderQueue(14);
+        queue.addOrder("Richard", "March 25, 2025", 5);
+        queue.addOrder("Petunia", "March 25, 2025", 5);
+        queue.addOrder("Samuel", "March 25, 2025", 8);
+        System.out.println(queue.toString());
+
+        queue.sellStock();
+        System.out.println(queue.toString());
+
+        queue.addStock(10);
+        queue.addOrder("Elvira", "March 25, 2025", 3);
+        queue.addOrder("Melusina", "March 25, 2025", 3);
+        queue.addOrder("Magdalena", "March 25, 2025", 5);
+        queue.sellStock();
+        System.out.println(queue.toString());
+    }
 }
