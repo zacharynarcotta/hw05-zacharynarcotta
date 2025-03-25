88939fe7790d75baeae30067c264b1ddd90743c5
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-25 18:39:04 -0400

    Create constructors

--- OrderQueue.java
+++ OrderQueue.java
@@ -1,7 +1,12 @@
 public class OrderQueue {
     public LinkedQueue<CustomerOrder> queue = new LinkedQueue<CustomerOrder>();
     public int stock;
-    public int sizeOfQueue;
+    public int sizeOfQueue = 0;
 
-    
+    public OrderQueue() {
+        stock = 0;
+    }
+    public OrderQueue(int stock) {
+        this.stock = stock;
+    }
 }
