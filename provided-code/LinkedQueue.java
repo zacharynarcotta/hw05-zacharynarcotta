716c5559dbe710bd6e656b241c84510cc96ff045
zacharynarcotta <zachary.narcotta@maine.edu>
2025-03-26 14:48:06 -0400

    Modify LinkedQueue code to match in-class demo

--- provided-code/LinkedQueue.java
+++ provided-code/LinkedQueue.java
@@ -1,6 +1,8 @@
 public class LinkedQueue<T> implements Queue<T> {
 
     private LinkedNode<T> head; 
+    private LinkedNode<T> tail;
+    private int size = 0;
 
     public LinkedQueue() {
         head = null;
@@ -8,52 +10,51 @@ public class LinkedQueue<T> implements Queue<T> {
 
     public void enqueue(T element) {
         if(head == null) {
-            head = new LinkedNode<T>(element); 
+            head = new LinkedNode<T>(element);
+            tail = head; 
+            size = 1;
         }
         else {
-            LinkedNode<T> temp = head;
-            while(temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = new LinkedNode<T>(element);
+            tail.next = new LinkedNode<T>(element);
+            tail = tail.next; 
+            size++;
         }
     }
 
-    // O(1)
     public T dequeue() {
         if(head == null) {
             return null;
         }
+        else if(head == tail) {
+            LinkedNode<T> temp = head;
+            head = head.next;
+            tail = null;
+            size--;
+            return temp.element;
+        }
         else {
             LinkedNode<T> temp = head;
             head = head.next;
+            size--;
             return temp.element;
         }
     }
 
-    // O(1)
     public T getFront() {
-        return head.element;
+        if(!isEmpty()) {
+            return head.element;
+        }
+        else {
+            return null;
+        } 
     }
 
-    // O(1)
     public boolean isEmpty() {
         return head == null;
     }
 
     public int size() {
-        int x = 1;
-        if(this.isEmpty()) {
-            return 0;
-        }
-        else {
-            LinkedNode<T> temp = head;
-            while(temp.next != null) {
-                x++;
-                temp = temp.next;
-            }
-            return x;
-        }
+        return size;
     }
     
 }
